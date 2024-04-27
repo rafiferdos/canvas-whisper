3baf81dd3ead55e74106efbac482d68e94bf3f27
Rafi Ferdos <rafiferdos@gmail.com>
2024-04-27 12:09:23 +0600

    Add Dark mode, update tailwind.config.js and index.html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,34 @@
 import { Link, NavLink } from "react-router-dom";
 import { IoPersonOutline } from "react-icons/io5";
 import { IoPersonAddOutline } from "react-icons/io5";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../FirebaseProvider/FirebaseProvider";
 
 
 
 const Navbar = () => {
 
-    const {user, logout} = useContext(AuthContext)
+    const { user, logout } = useContext(AuthContext)
+
+    const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
+    const toggleTheme = (e) => {
+        if (e.target.checked)
+            setTheme('dark')
+        else
+            setTheme('light')
+    }
+    useEffect(() => {
+        localStorage.setItem('theme', theme)
+        const localTheme = localStorage.getItem('theme')
+        document.querySelector('html').setAttribute('data-theme', localTheme);
+    }, [theme]);
 
     const links =
         <>
-            <NavLink to='/' className={({ isActive }) => isActive ? "text-purple-700 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-400"}><a>Home</a></NavLink>
-            <NavLink to='/all_art_and_craft_items' className={({ isActive }) => isActive ? "text-purple-700 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-400"}><a>All Art & Craft Items</a></NavLink>
-            <NavLink to='/add_cart_item' className={({ isActive }) => isActive ? "text-purple-700 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-400"}><a>Add Cart Item</a></NavLink>
-            <NavLink to='/my_art_and_craft_list' className={({ isActive }) => isActive ? "text-purple-700 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-400"}><a>My Art & Craft List</a></NavLink>
+            <NavLink to='/' className={({ isActive }) => isActive ? "text-purple-500 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-300"}><a>Home</a></NavLink>
+            <NavLink to='/all_art_and_craft_items' className={({ isActive }) => isActive ? "text-purple-500 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-300"}><a>All Art & Craft Items</a></NavLink>
+            <NavLink to='/add_cart_item' className={({ isActive }) => isActive ? "text-purple-500 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-300"}><a>Add Cart Item</a></NavLink>
+            <NavLink to='/my_art_and_craft_list' className={({ isActive }) => isActive ? "text-purple-500 lg:border-b-purple-500 lg:border-b-2" : "hover:text-purple-300"}><a>My Art & Craft List</a></NavLink>
         </>
     return (
         <>
@@ -34,10 +47,10 @@ const Navbar = () => {
                             {links}
                         </ul>
                     </div>
-                    <div className="absolute ml-14 lg:ml-0 overflow-hidden flex justify-center items-center">
-                    {/* bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient bg-300%  */}
-                    <video className="absolute object-cover w-full" autoPlay loop muted> <source src="https://videos.pexels.com/video-files/10914360/10914360-hd_1920_1080_24fps.mp4" /></video>
-                    <a className="text-lg lg:text-3xl md:text-xl font-pacifico bg-white mix-blend-screen">Canvas Whisper</a>
+                    <div className="absolute ml-14 lg:ml-0 overflow-hidden flex justify-center items-center rounded-2xl border-none">
+                        {/* bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient bg-300%  */}
+                        <video className="absolute " autoPlay loop muted> <source src="https://videos.pexels.com/video-files/10914360/10914360-hd_1920_1080_24fps.mp4" /></video>
+                        <a className={theme === 'light' ? 'text-lg lg:text-3xl md:text-xl font-pacifico bg-white mix-blend-screen' : 'text-lg lg:text-3xl md:text-xl font-pacifico bg-black mix-blend-hard-light'}>Canvas Whisper</a>
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
@@ -61,23 +74,28 @@ const Navbar = () => {
                             </div>
                             :
                             <>
-                            
-                            <Link
-                                to='/login'
-                                className="btn shadow-none bg-white text-purple-700 hover:bg-purple-100 btn-primary btn-xs sm:btn-sm border-none md:btn-md lg:btn-md rounded-2xl"
-                                data-aos="zoom-in-left"
-                            >
-                                <IoPersonOutline />Login
-                            </Link>
-                            <Link
-                                to='/register'
-                                className="btn shadow-none bg-white text-orange-600 hover:bg-orange-100 btn-primary btn-xs sm:btn-sm border-none md:btn-md lg:btn-md hidden md:flex rounded-2xl"
-                                data-aos="zoom-in-left"
-                            >
-                                <IoPersonAddOutline />Register
-                            </Link>
+
+                                <Link
+                                    to='/login'
+                                    className={theme === 'light' ? 'btn shadow-none bg-white text-purple-700 hover:bg-purple-100 btn-primary btn-xs sm:btn-sm border-none md:btn-md lg:btn-md rounded-2xl' : 'btn shadow-none bg-transparent text-purple-400 hover:bg-purple-950 hover:text-purple-300 btn-primary btn-xs sm:btn-sm border-none md:btn-md lg:btn-md rounded-2xl'}
+                                    data-aos="zoom-in-left"
+                                >
+                                    <IoPersonOutline />Login
+                                </Link>
+                                <Link
+                                    to='/register'
+                                    className={theme === 'light' ? "btn shadow-none bg-white text-orange-600 hover:bg-orange-100 btn-primary btn-xs sm:btn-sm border-none md:btn-md lg:btn-md hidden md:flex rounded-2xl" : "btn shadow-none bg-transparent text-orange-400 hover:bg-orange-950 btn-primary btn-xs sm:btn-sm border-none md:btn-md lg:btn-md hidden md:flex rounded-2xl"}
+                                    data-aos="zoom-in-left"
+                                >
+                                    <IoPersonAddOutline />Register
+                                </Link>
                             </>
                     }
+                    <label className="cursor-pointer grid place-items-center ml-2">
+                        <input type="checkbox" onChange={toggleTheme} value={theme} className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" checked={theme === 'light' ? false : true} />
+                        <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                        <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                    </label>
                 </div>
             </div>
         </>
