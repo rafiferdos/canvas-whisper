63fd67122f619ea560e556f2267ce4a0ef61ecec
Rafi Ferdos <rafiferdos@gmail.com>
2024-04-30 10:54:32 +0600

    Add loader, update FirebaseProvider usage and modify Navbar component

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -4,11 +4,14 @@ import { AuthContext } from "../FirebaseProvider/FirebaseProvider";
 
 
 // eslint-disable-next-line react/prop-types
-const PrivateRoutes = ({children}) => {
+const PrivateRoutes = ({ children }) => {
     const location = useLocation()
 
-    const {user} = useContext(AuthContext)
-    
+    const { user, loading } = useContext(AuthContext)
+
+    if (loading)
+        return <div className="min-h-screen flex items-center justify-center"><span className="loading loading-infinity loading-lg text-primary"></span></div>
+
     if (!user)
         return <Navigate to='/login' state={location?.pathname || '/'} />
     return children
