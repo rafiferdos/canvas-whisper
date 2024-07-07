6549f61624d700ad5b742943edfbbc416d885f43
Rafi Ferdos <rafiferdos@gmail.com>
2024-07-07 14:17:09 +0600

    refactor: Update CraftItems and AllArtAndCraftItems components
    - Add className "object-cover" to the img tag for better image display
    - Add price and email information for each item
    - Update button styling with rounded corners
    
    Related to recent user commits:
    - Refactor AddCraftItem component, update FirebaseProvider usage, and modify Navbar component
    - Update FirebaseProvider usage, modify Navbar component, and add loader to AllArtAndCraftItems
    - Add loader, update FirebaseProvider usage and modify Navbar component
    - Update AddCartItem component to AddCraftItem, update login and register functionality
    - Update Navbar component to use Link instead of anchor tag

--- src/components/CraftItems.jsx
+++ src/components/CraftItems.jsx
@@ -10,16 +10,18 @@ const CraftItems = ({ allData }) => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mb-4 md:mb-8 lg:mb-16 ">
                     {
                         // eslint-disable-next-line react/prop-types
-                        allData.slice(0,6).map((item) => {
+                        allData.slice(0, 6).map((item) => {
                             return (
                                 <>
                                     <div key={item._id} className="card lg:w-96 bg-base-300 shadow-xl">
-                                        <figure><img src={item?.image_url} alt="Shoes" /></figure>
+                                        <figure><img className="object-cover" src={item?.image_url} alt="Shoes" referrerPolicy="no-referrer" onError={(e) => { e.target.onerror = null; e.target.src = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg' }} /></figure>
                                         <div className="card-body">
                                             <h2 className="card-title">{item.item_name}</h2>
+                                            <h2 className="opacity-70">Price: ${item.price}</h2>
                                             <p>{item.short_description}</p>
+                                            <h2 className="opacity-70">By: {item.email}</h2>
                                             <div className="card-actions justify-end">
-                                                <Link to={`/art_and_crafts/details/${item._id}`} className="btn btn-primary">View Details</Link>
+                                                <Link to={`/art_and_crafts/details/${item._id}`} className="btn btn-primary rounded-3xl">View Details</Link>
                                             </div>
                                         </div>
                                     </div>
